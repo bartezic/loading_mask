e52f435113896439df8170887a4bca66da5acd6e
Viktor Oleksyn <bartezic@gmail.com>
2015-09-22 09:32:52 +0300

    fix for remove mask

--- vendor/assets/javascripts/loading_mask.js
+++ vendor/assets/javascripts/loading_mask.js
@@ -7,7 +7,7 @@
       if($(this).is('body') || $(this).is('html')){ cls += ' fixed' };
       $('<div>', {class: cls}).appendTo($(this));
     } else if (action === 'hide') {
-      $(this).find('.'+cls).remove();
+      $(this).children('.'+cls).remove();
     }
   };
 }(jQuery));
